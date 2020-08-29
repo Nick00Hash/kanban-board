d3953fbf66f011aa4c2e4179ff26fb4ed8765142
Andrew Higgins <andrewhiggins27@gmail.com>
2020-08-29 13:38:44 -0400

    working card dnd

--- src/ui/CardTile.js
+++ src/ui/CardTile.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
+import { Draggable } from 'react-beautiful-dnd'
 
 const useStyles = makeStyles({
   root: {
     minWidth: 275,
+  },
+  rootColor: {
+    minWidth: 275,
+    backgroundColor: 'lightgreen'
   },
   bullet: {
     display: 'inline-block',
@@ -24,27 +29,37 @@ const useStyles = makeStyles({
   const bull = <span className={classes.bullet}>•</span>;
 
   return (
-    <Card className={classes.root}>
-      <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          {props.card.title}
-        </Typography>
-        <Typography variant="h5" component="h2">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          adjective
-        </Typography>
-        <Typography variant="body2" component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+    <Draggable draggableId={props.card.title} index={props.index}>
+      {(provided, snapshot) => (
+        <div
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+        >
+          <Card className={ (snapshot.isDragging ? classes.rootColor : classes.root) }>
+            <CardContent>
+              <Typography className={classes.title} color="textSecondary" gutterBottom>
+                {props.card.title}
+              </Typography>
+              <Typography variant="h5" component="h2">
+                be{bull}nev{bull}o{bull}lent
+              </Typography>
+              <Typography className={classes.pos} color="textSecondary">
+                adjective
+              </Typography>
+              <Typography variant="body2" component="p">
+                well meaning and kindly.
+                <br />
+                {'"a benevolent smile"'}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
+        </div>
+      )}
+    </Draggable>
   );
 }
 
--- src/ui/Column.js
+++ src/ui/Column.js
@@ -1,19 +1,47 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import { Droppable } from 'react-beautiful-dnd'
 import { Grid } from "@material-ui/core"
 
 import CardTile from './CardTile'
 
+const useStyles = makeStyles({
+  column: {
+    minHeight: '200px'
+  },
+  columnColored: {
+    minHeight: '200px',
+    transition: 'background-color 0.3s ease',
+    backgroundColor: 'yellow'
+  }
+});
+
 const Column = (props) => {
-  const mappedCards = props.column.cards.map(card=> {
+  const classes = useStyles();
+
+  const mappedCards = props.column.cards.map((card, index) => {
     return (
-      <CardTile key={card.title} card={card} />
+      <CardTile key={card.title} card={card} index={index} />
     )
   })
+  
   return (
     <div>
-      {mappedCards}
+      <Droppable droppableId={props.column.columnId}>
+        {(provided, snapshot) => (
+          <div
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+            className={(snapshot.isDraggingOver ? classes.columnColored : classes.column)}
+          >
+            {mappedCards}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
     </div>
   )
 }
 
 export default Column
+ 
